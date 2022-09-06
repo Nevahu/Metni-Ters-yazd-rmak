a6c00c1e5295a592cebe6c69450c17932d8c530d
Nevahu <nevahuarslan26@gmail.com>
2022-09-06 07:59:25 +0300

    Metni Ters yazdırmak

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int ters()
+{
+	char metin;
+	scanf("%c", &metin);
+
+	if (metin != '\n')
+	{
+		ters();
+
+		printf("%c", metin);
+	}
+}
+
+int main()
+{
+	printf("Lutfen Metninizi Giriniz: \n");
+
+	ters();
+
+	return 0;
+}
